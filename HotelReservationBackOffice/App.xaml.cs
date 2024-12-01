c58f4f3c5cdc7a5d7d912b413975ceeeadc178ee
Youssef <youssefnaib46@gmail.com>
2024-12-01 23:03:28 +0100

    Initial commit

---
+++ HotelReservationBackOffice/App.xaml.cs
@@ -0,0 +1,13 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace HotelReservationBackOffice;
+
+/// <summary>
+/// Interaction logic for App.xaml
+/// </summary>
+public partial class App : Application
+{
+}
+
